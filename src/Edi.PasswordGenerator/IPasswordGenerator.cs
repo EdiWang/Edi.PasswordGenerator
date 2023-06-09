d38ac76847b21f2161ffbe454424eafad4d0e09b
Edi Wang <Edi.Wang@outlook.com>
2023-06-09 10:05:25 +0800

    PasswordRule

--- src/Edi.PasswordGenerator/IPasswordGenerator.cs
+++ src/Edi.PasswordGenerator/IPasswordGenerator.cs
@@ -8,4 +8,18 @@ namespace Edi.PasswordGenerator;
 
 public interface IPasswordGenerator
 {
+    string GeneratePassword(PasswordRule rule = null);
+}
+
+public class PasswordRule
+{
+    public PasswordRule(int length, int numberOfNonAlphanumericCharacters)
+    {
+        Length = length;
+        NumberOfNonAlphanumericCharacters = numberOfNonAlphanumericCharacters;
+    }
+
+    public int Length { get; set; }
+
+    public int NumberOfNonAlphanumericCharacters { get; set; }
 }
