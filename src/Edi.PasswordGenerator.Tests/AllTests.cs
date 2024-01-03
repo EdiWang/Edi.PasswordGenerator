1e37f428e01225be3c5dc787841a3a2150d8f8be
Edi Wang <Edi.Wang@outlook.com>
2024-01-03 09:33:21 +0800

    migrate NUnit to MSTest

--- src/Edi.PasswordGenerator.Tests/AllTests.cs
+++ src/Edi.PasswordGenerator.Tests/AllTests.cs
@@ -1,93 +1,85 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Edi.PasswordGenerator.Tests;
 
-[TestFixture]
+[TestClass]
 public class Tests
 {
-    // Create unit test for DefaultPasswordGenerator
-    [Test]
+    [TestMethod]
     public void TestGeneratePasswordNoRule()
     {
         var generator = new DefaultPasswordGenerator();
         var password = generator.GeneratePassword();
 
-        Assert.That(password, Is.Not.Null);
-        Assert.That(password, Is.Not.Empty);
+        Assert.IsNotNull(password);
+        Assert.IsFalse(string.IsNullOrWhiteSpace(password));
 
         // Assert if password has both upper and lower case letters
-        Assert.That(password.Any(char.IsUpper), Is.True);
-        Assert.That(password.Any(char.IsLower), Is.True);
+        Assert.IsTrue(password.Any(char.IsUpper));
+        Assert.IsTrue(password.Any(char.IsLower));
 
         // Assert if password has at least one digit
-        Assert.That(password.Any(char.IsDigit), Is.True);
+        Assert.IsTrue(password.Any(char.IsDigit));
 
         // Assert if password has at least one special character
-        Assert.That(password.Any(char.IsPunctuation), Is.True);
-
-        Assert.Pass();
+        Assert.IsTrue(password.Any(char.IsPunctuation));
     }
 
-    [TestCase(5)]
-    [TestCase(10)]
-    [TestCase(15)]
+    [TestMethod]
+    [DataRow(5)]
+    [DataRow(10)]
+    [DataRow(15)]
     public void TestGeneratePasswordRequiredLength(int length)
     {
         var generator = new DefaultPasswordGenerator();
         var password = generator.GeneratePassword(new(length, 0));
 
-        Assert.That(password, Is.Not.Null);
-        Assert.That(password, Is.Not.Empty);
+        Assert.IsNotNull(password);
+        Assert.IsFalse(string.IsNullOrWhiteSpace(password));
 
-        Assert.That(password.Length, Is.EqualTo(length));
-
-        Assert.Pass();
+        Assert.AreEqual(password.Length, length);
     }
 
-    [TestCase(2)]
-    [TestCase(3)]
+    [TestMethod]
+    [DataRow(2)]
+    [DataRow(3)]
     public void TestGeneratePasswordLeastNum(int num)
     {
         var generator = new DefaultPasswordGenerator();
         var password = generator.GeneratePassword(new(10, num));
 
-        Assert.That(password, Is.Not.Null);
-        Assert.That(password, Is.Not.Empty);
-        Assert.That(password.Length, Is.EqualTo(10));
+        Assert.IsNotNull(password);
+        Assert.IsFalse(string.IsNullOrWhiteSpace(password));
+        Assert.AreEqual(10, password.Length);
 
         char[] extendedPunctuations = "!@#$%^&*()_-+=[{]};:>|./?".ToCharArray();
 
         // Assert if password has at least num of character in extendedPunctuations
-        Assert.That(password.Count(c => extendedPunctuations.Contains(c)) >= num, Is.True);
-
-        Assert.Pass();
+        Assert.IsTrue(password.Count(c => extendedPunctuations.Contains(c)) >= num);
     }
 
-    [TestCase(0)]
-    [TestCase(129)]
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(129)]
     public void TestGeneratePasswordInvalidLength(int length)
     {
         var generator = new DefaultPasswordGenerator();
 
-        Assert.Throws(typeof(ArgumentOutOfRangeException), () =>
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
         {
             var password = generator.GeneratePassword(new(length, 0));
         });
     }
 
-    [TestCase(-1)]
-    [TestCase(-10)]
-    [TestCase(11)]
+    [TestMethod]
+    [DataRow(-1)]
+    [DataRow(-10)]
+    [DataRow(11)]
     public void TestGeneratePasswordInvalidNumberOfNonAlphanumericCharactersLength(int length)
     {
         var generator = new DefaultPasswordGenerator();
 
-        Assert.Throws(typeof(ArgumentOutOfRangeException), () =>
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
         {
             var password = generator.GeneratePassword(new(10, length));
         });
