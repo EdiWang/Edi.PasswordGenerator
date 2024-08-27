421ac2389ae2eb5b496c41491829268e276346ab
Edi Wang <Edi.Wang@outlook.com>
2024-08-27 14:34:40 +0800

    Refactor password generation to enhance security
    Removed System.Security.Cryptography import and GeneratePasswordDefault method from PasswordGenerator.cs. Updated DefaultPasswordGenerator to use SecurePasswordGenerator. Added SecurePasswordGenerator class with methods to generate secure passwords, ensuring inclusion of various character types and randomness.

--- src/Edi.PasswordGenerator/PasswordGenerator.cs
+++ src/Edi.PasswordGenerator/PasswordGenerator.cs
@@ -1,40 +1,11 @@
-﻿using System.Security.Cryptography;
-
-namespace Edi.PasswordGenerator;
+﻿namespace Edi.PasswordGenerator;
 
 public class DefaultPasswordGenerator : IPasswordGenerator
 {
     public string GeneratePassword(PasswordRule rule = null)
     {
-        return rule == null ? GeneratePasswordDefault() : AspNetMembershipPasswordGenerator.GeneratePassword(rule.Length, rule.LeastNumberOfNonAlphanumericCharacters);
-    }
-
-    private static string GeneratePasswordDefault()
-    {
-        string engLower = "aquickbrownfoxjumpedoverthelazydog";
-        var engUpperArray = engLower.ToUpper().ToArray();
-
-        var letters = engLower.ToArray().Concat(engUpperArray).ToArray();
-        var nums = Enumerable.Range(0, 10).ToArray();
-        var chars = new[] { '@', '$', '!', '%', '*', '#', '?', '&' };
-
-        var pwdArray = new char[10];
-        for (var i = 0; i < 6; i++)
-        {
-            var posL = RandomNumberGenerator.GetInt32(0, letters.Length);
-            pwdArray[i] = (letters[posL]);
-        }
-        for (var i = 0; i < 3; i++)
-        {
-            var posN = RandomNumberGenerator.GetInt32(0, nums.Length);
-            pwdArray[6 + i] = (char)(nums[posN] + 48);
-        }
-        var posC = RandomNumberGenerator.GetInt32(0, chars.Length);
-        pwdArray[9] = chars[posC];
-
-        var rndArray = pwdArray.OrderBy(p => Guid.NewGuid()).ToArray();
-        var password = new string(rndArray);
-
-        return password;
+        return rule == null ? 
+            SecurePasswordGenerator.GenerateSecurePassword() : 
+            AspNetMembershipPasswordGenerator.GeneratePassword(rule.Length, rule.LeastNumberOfNonAlphanumericCharacters);
     }
 }
---
+++ src/Edi.PasswordGenerator/SecurePasswordGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+
+namespace Edi.PasswordGenerator;
+
+public class SecurePasswordGenerator
+{
+    private static readonly char[] UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    private static readonly char[] LowercaseLetters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+    private static readonly char[] Digits = "0123456789".ToCharArray();
+    private static readonly char[] SpecialCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?".ToCharArray();
+
+    public static string GenerateSecurePassword(int length = 12)
+    {
+        if (length < 8)
+        {
+            throw new ArgumentException("Password length must be at least 8 characters.");
+        }
+
+        var charCategories = new[]
+        {
+            UppercaseLetters,
+            LowercaseLetters,
+            Digits,
+            SpecialCharacters
+        };
+
+        var passwordChars = new char[length];
+
+        // Ensure the password contains at least one character from each category
+        for (int i = 0; i < charCategories.Length; i++)
+        {
+            passwordChars[i] = GetRandomCharFromCategory(charCategories[i]);
+        }
+
+        // Fill the remaining characters with random characters from all categories
+        for (int i = charCategories.Length; i < length; i++)
+        {
+            var randomCategory = charCategories[GetRandomNumber(0, charCategories.Length)];
+            passwordChars[i] = GetRandomCharFromCategory(randomCategory);
+        }
+
+        // Shuffle the characters to ensure randomness
+        passwordChars = passwordChars.OrderBy(c => GetRandomNumber(0, length)).ToArray();
+
+        return new string(passwordChars);
+    }
+
+    private static char GetRandomCharFromCategory(char[] category)
+    {
+        return category[GetRandomNumber(0, category.Length)];
+    }
+
+    private static int GetRandomNumber(int minValue, int maxValue)
+    {
+        byte[] randomNumber = new byte[4];
+        RandomNumberGenerator.Fill(randomNumber);
+        int value = BitConverter.ToInt32(randomNumber, 0);
+        return Math.Abs(value % (maxValue - minValue)) + minValue;
+    }
+}
